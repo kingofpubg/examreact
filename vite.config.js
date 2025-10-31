417af6f43822e902411426a542f5207734513d5c
kingofpubg <giahuy04041999@gmail.com>
2025-10-31 19:09:56 +0700

    up

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/examreact/',   // 👈 cực kỳ quan trọng
   plugins: [react()],
 })
